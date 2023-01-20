1e9d6e18e4d27116a98c22c9cb1d11fd66fd2c23
cailloutr <caio.trocilo@gmail.com>
2023-01-20 16:45:48 -0300

    Cria projeto e implementa as dependências do firebase

---
+++ app/src/main/java/com/example/totalfit/ui/home/HomeFragment.kt
@@ -0,0 +1,34 @@
+package com.example.totalfit.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.totalfit.databinding.FragmentLoginBinding
+
+
+class HomeFragment : Fragment() {
+
+    var _binding: FragmentLoginBinding? = null
+    val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
